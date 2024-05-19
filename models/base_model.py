02cd2c906f8a241da481e0300f8bea2716e02a4c
MSB0095 <andromedianmsb01@gmail.com>
2024-05-20 00:10:12 +0100

    Modifier la méthode __str__ de la classe BaseModel pour inclure le nom de la classe dans la chaîne de caractères retournée

--- models/base_model.py
+++ models/base_model.py
@@ -3,21 +3,36 @@ import datetime
 
 
 class BaseModel:
+    """
+    BaseModel class
+    """
     def __init__(self) -> None:
+        """
+        init method
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
         return
 
     def __str__(self) -> str:
+        """
+        str method
+        """
         classname = self.__class__.__name__
         return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self) -> None:
+        """
+        save method - updates the updated_at date
+        """
         self.updated_at = datetime.datetime.now()
         return
 
     def to_dict(self) -> dict:
+        """
+        to_dict method - returns a dictionnary of the object
+        """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = dictionary["created_at"].isoformat()
