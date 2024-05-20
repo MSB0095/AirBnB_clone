3049f8a7db2a52eb47c5b0293f5202ea83e087a2
MSB0095 <andromedianmsb01@gmail.com>
2024-05-20 23:51:12 +0100

    Modifier la méthode __init__ de la classe BaseModel pour prendre des arguments positionnels et des arguments par mot-clé

--- models/base_model.py
+++ models/base_model.py
@@ -17,16 +17,15 @@ class BaseModel:
         init method
         it runs at the creation of each instance
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        if kwargs is not None:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.key = datetime.datetime.fromisoformat(value)
-                else:
-                    self.key = value
-        return
+                    value = datetime.datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.datetime.now()
 
     def __str__(self) -> str:
         """
